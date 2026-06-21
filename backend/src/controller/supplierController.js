66a9f7494bbebce34a3da2ae67fe61bc4acda4e5
kavidu717 <dushmanthakavidu143@gmail.com>
2026-06-21 18:54:10 +0530

    feat(supplier): implement supplier controller

---
+++ backend/src/controller/supplierController.js
@@ -0,0 +1,32 @@
+import pool from "../config/db.js";
+
+const addSupplier =async(req,res)=>{
+    try{
+
+        const {name,contact_number,address}=req.body;
+
+        const newSupplier=await pool.query("INSERT INTO suppliers(name,contact_number,address) VALUES($1,$2,$3) RETURNING *",[name,contact_number,address]);
+
+        res.status(201).json({
+            message:"supplier added successfully",
+            supplier:newSupplier.rows[0],
+        });
+
+
+
+    }catch(error){
+           console.error(err.message);
+        res.status(500).json({ error: "Server error" });
+    }
+}
+
+ export const getSuppliers=async(req,res)=>{
+    try{
+        const suppliers=await pool.query("SELECT * FROM suppliers ORDER BY id DESC");
+        res.json(allSuppliers.rows);
+        
+    }catch(error){
+        console.error(err.message);
+        res.status(500).json({ error: "Server error" });
+    }
+ }
