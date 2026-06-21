dd836bcc92341a49c395574b2580473814571eb2
kavidu717 <dushmanthakavidu143@gmail.com>
2026-06-21 16:48:46 +0530

    feat(database): setup PostgreSQL connection using pg pool

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import pool from './src/config/db.js';
 
 const app = express();
 
@@ -9,7 +10,23 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
+ 
+app.get('/api/test', async(req, res) => {
+    try{
+        const result= await pool.query("SELECT NOW()")
+        res.json(
+            {
+                message: "backend and database are fully connected",
+                database_time: result.rows[0].now
+            }
+        );
 
+    }catch(error){
+        console.log(error);
+        res.status(500).
+        json({error: error.message});
+    }
+});
 
 
 
--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,5 +1,9 @@
-import pool from 'pg';
+import pkg from 'pg';
+
+const { Pool } = pkg;
 import dotenv from 'dotenv';
+
+dotenv.config();
 
 
 const pool = new Pool({
